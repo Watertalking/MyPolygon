051beae2078a32c7306080a485ebfb0b339a866b
Emerald <hec85d64@gmail.com>
2026-03-10 21:19:37 +0300

    Ревью index.php

--- index.php
+++ index.php
@@ -1,13 +1,17 @@
 <?php
 $product = [
-    'title' => 'Футболка',
+    'name' => 'Футболка',
     'price' => 1000,
-    'count' => 5
+    'stock' => 5
 ];
 
-if ($product['count'] > 0 && $product['price'] < 1500) {
-    $product['price'] -= $product['price'] * 0.1;
-    echo "Товар {$product['title']} доступен для заказа. Цена со скидкой: {$product['price']}";
+$priceThreshold = 1500;
+$discountRate = 10;
+
+if ($product['stock'] > 0 && $product['price'] < $priceThreshold) {
+    $discountAmount = ($product['price'] * $discountRate) / 100;
+    $finalPrice = $product['price'] - $discountAmount;
+    echo "Товар {$product['name']} (оригинальная цена: {$product['price']} руб.) доступен для заказа. Ваша цена со скидкой: {$finalPrice} руб.";
 }else{
     echo "Товара нет в наличии или он слишком дорогой";
 }
