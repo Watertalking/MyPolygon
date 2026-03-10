7c534285db11520320e7ab1072f089a261833fe9
Emerald <hec85d64@gmail.com>
2026-03-10 21:04:48 +0300

    Проверка редактора

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
 <?php
+$product = [
+    'title' => 'Футболка',
+    'price' => 1000,
+    'count' => 5
+];
+
+if ($product['count'] > 0 && $product['price'] < 1500) {
+    $product['price'] -= $product['price'] * 0.1;
+    echo "Товар {$product['title']} доступен для заказа. Цена со скидкой: {$product['price']}";
+}else{
+    echo "Товара нет в наличии или он слишком дорогой";
+}
