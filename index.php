8f532260d90411a68a6fb7bd4a8e0648e907ded0
Emerald <hec85d64@gmail.com>
2026-03-11 13:18:23 +0300

    Практика функций

--- index.php
+++ index.php
@@ -1,58 +1,27 @@
 <?php
 
-$tasks = [
-    'побриться',
-    'позавтракать',
-    'прогреть авто',
-    'зарядить смартфон',
-    'проверить почту',
-];
+function greetUser($name = 'гость'): string
+{
+    return "Привет, $name!";
+}
 
-$tasks_with_priority = [
-    [
-        'title' =>'побриться',
-        'priority' => 2
-    ],
-    [
-        'title' =>'позавтракать',
-        'priority' => 3
-    ],
-    [
-        'title' =>'прогреть авто',
-        'priority' => 2
-    ],
-    [
-        'title' =>'зарядить смартфон',
-        'priority' => 3
-    ],
-    [
-        'title' =>'проверить почту',
-        'priority' => 1
-    ],
-];
+function validateEmail($email): string
+{
+    if (strlen($email) < 5) {
+        return false;
+    }
+    $pattern = '/^[^@]+@[^@]+\.[^@]+$/';
+    return preg_match($pattern, $email) === 1;
+}
 
-?>
-<ul>
-    <?php foreach ($tasks as $task):?>
-        <li><?=$task?></li>
-    <?php endforeach;?>
-</ul>
+function formatUserData($name, $email): array
+{
+    $formatName = mb_convert_case($name, MB_CASE_TITLE, 'UTF-8');
+    $formatEmail = strtolower($email);
+    return [
+      'formatted_name' => $formatName,
+      'formatted_email' => $formatEmail
+    ];
+}
 
-<table>
-    <tr><th>Задача</th><th>Приоритет</th></tr>
-    <?php foreach ($tasks_with_priority as $task):?>
-    <tr>
-        <td><?=$task['title']?></td>
-        <?php
-        $priority_map = [
-            1 => 'Низкий',
-            2 => 'Средний',
-            3 => 'Высокий'
-        ];
-
-        $priority = $priority_map[$task['priority']] ?? 'Без приоритета';
-        ?>
-        <td><?=$priority?></td>
-    </tr>
-    <?php endforeach;?>
-</table>
+var_dump(formatUserData("вЛАДИМИР", "TEST@MAIL.RU"));
